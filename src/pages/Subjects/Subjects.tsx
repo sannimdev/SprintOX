c79ed577daa0b27338283edf19da44f4db381183
Hojoong Choi <inegg.apps@gmail.com>
2024-02-11 18:44:30 +0900

    feat: 과목 선택 화면 스타일링

--- src/pages/Subjects/Subjects.tsx
+++ src/pages/Subjects/Subjects.tsx
@@ -1,12 +1,26 @@
 import { useAtom } from 'jotai';
 import { quizzesAtom } from '../../atoms';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  h1 {
+    font-size: 1.6rem;
+    font-weight: bold;
+    text-align: center;
+    margin: 32px 0;
+  }
+
+  ul > li:not(:last-child) {
+    margin-bottom: 16px;
+  }
+`;
 
 function Subjects() {
   const [quizzes] = useAtom(quizzesAtom);
 
   return (
-    <div>
+    <Container>
       <h1>과목 선택</h1>
       <ul>
         {quizzes.map(({ key, label }) => (
@@ -17,7 +31,7 @@ function Subjects() {
           </li>
         ))}
       </ul>
-    </div>
+    </Container>
   );
 }
 
