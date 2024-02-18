a60d78a4697745d067452917f4b0661b02d1bfd7
Hojoong Choi <inegg.apps@gmail.com>
2024-02-18 14:27:16 +0900

    feat: 브라우저 라우팅 방식 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createMemoryRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import './reset.css';
 import GlobalStyle from './GlobalStyle.ts';
@@ -8,7 +8,7 @@ import Subjects from './pages/Subjects/Subjects.tsx';
 import App from './App.tsx';
 import Questions from './pages/Questions/Questions.tsx';
 
-const router = createBrowserRouter([
+const router = createMemoryRouter([
   {
     path: '/',
     element: <Subjects />,
